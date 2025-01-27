5c989eba953fe4fc2b8b622015a9bdc2280b7b4c
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2025-01-27 11:11:53 +0100

    feat : added more test to the authentication endpoints

--- roam_mate_server/test/integration/auth_endpoint/auth_endpoint_test.dart
+++ roam_mate_server/test/integration/auth_endpoint/auth_endpoint_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:roam_mate_server/src/generated/protocol.dart';
 import 'package:roam_mate_server/src/utils/map_auth_exception_to_error_message.dart';
 import 'package:test/test.dart';
@@ -122,36 +120,165 @@ void main() {
       },
     );
 
-    // group("Sign up", () {
-    //   test(
-    //     "Test that account is created successfully and otp is sent",
-    //     () async {
-    //       final session = sessionBuilder.build();
-    //       final user = await endpoints.auth.createAccount(
-    //         sessionBuilder,
-    //         email,
-    //         password,
-    //         username,
-    //         scope,
-    //       );
-    //       final storedUser = await User.db.findFirstRow(
-    //         session,
-    //         where: (t) => t.email.equals(email),
-    //       );
+    group(
+      "Sign Up",
+      () {
+        test(
+          "test that user creates account successfully and otp is sent",
+          () async {
+            await endpoints.auth.createAccount(
+              sessionBuilder,
+              email,
+              password,
+              username,
+              scope,
+            );
 
-    //       expect(storedUser, isNotNull);
-    //       expect(storedUser!.id, equals(user.id));
-    //       expect(storedUser.email, equals(email));
-    //       expect(storedUser.password, isNot(password));
-    //       expect(storedUser.verified, isFalse);
-    //       expect(storedUser.username, username);
-    //       expect(storedUser.scope, scope);
+            final storedUser = await User.db.findFirstRow(
+              sessionBuilder.build(),
+              where: (t) => t.email.equals(email),
+            );
 
-    //       final otpVerification = await OtpVerification.db
-    //           .findFirstRow(session, where: (t) => t.email.equals(email));
-    //       expect(otpVerification, isNotNull);
-    //     },
-    //   );
-    // });
+            final storedOtp = await OtpVerification.db.findFirstRow(
+              sessionBuilder.build(),
+              where: (t) => t.email.equals(email),
+            );
+
+            expect(storedUser, isNotNull);
+            expect(storedUser!.email, equals(email));
+            expect(storedUser.password, isNot(password));
+            expect(storedUser.verified, isFalse);
+            expect(storedUser.username, username);
+            expect(storedUser.scope, scope);
+
+            expect(storedOtp, isNotNull);
+            expect(storedOtp!.expiredAt.isAfter(DateTime.now()), isTrue);
+          },
+        );
+
+        test(
+          "test that sign up endpoint throws an exeception when the username is in use",
+          () async {
+            final user = await endpoints.auth.createAccount(
+              sessionBuilder,
+              email,
+              password,
+              username,
+              scope,
+            );
+            await User.db.updateRow(
+              sessionBuilder.build(),
+              user.copyWith(verified: true),
+            );
+
+            expect(
+              () => endpoints.auth.createAccount(
+                sessionBuilder,
+                email,
+                password,
+                username,
+                scope,
+              ),
+              throwsA(
+                isA<AuthException>()
+                    .having(
+                      (e) => e.errorType,
+                      "error type",
+                      equals(AuthErrorType.usernameAlreadyExistsException),
+                    )
+                    .having(
+                      (e) => e.message,
+                      "error message",
+                      authTypeToMessage[
+                          AuthErrorType.usernameAlreadyExistsException],
+                    ),
+              ),
+            );
+          },
+        );
+
+        test(
+          "test that sign up throws an exception when email already exists",
+          () async {
+            final user = await endpoints.auth.createAccount(
+              sessionBuilder,
+              email,
+              password,
+              username,
+              scope,
+            );
+            await User.db.updateRow(
+              sessionBuilder.build(),
+              user.copyWith(verified: true),
+            );
+
+            expect(
+              () => endpoints.auth.createAccount(
+                sessionBuilder,
+                email,
+                password,
+                "anotherUsername",
+                scope,
+              ),
+              throwsA(
+                isA<AuthException>()
+                    .having(
+                      (e) => e.errorType,
+                      "error type",
+                      equals(AuthErrorType.emailAlreadyExistsException),
+                    )
+                    .having(
+                      (e) => e.message,
+                      "error message",
+                      authTypeToMessage[
+                          AuthErrorType.emailAlreadyExistsException],
+                    ),
+              ),
+            );
+          },
+        );
+      },
+    );
+
+    group("reset password", () {
+      setUp(() async {
+        final user = await endpoints.auth.createAccount(
+          sessionBuilder,
+          email,
+          password,
+          username,
+          scope,
+        );
+        await User.db.updateRow(
+          sessionBuilder.build(),
+          user.copyWith(verified: true),
+        );
+      });
+
+      test("test that initiate reset password sent an otp", () async {
+        final response =
+            await endpoints.auth.sendResetPasswordOtp(sessionBuilder, email);
+        final otpVerification = await OtpVerification.db.findFirstRow(
+          sessionBuilder.build(),
+          where: (t) => t.email.equals(email),
+        );
+        expect(response, isTrue);
+        expect(otpVerification, isNotNull);
+      });
+
+      test(
+        "test that initiate reset password throws an exception when the user doesn't exist",
+        () async {
+          await User.db.deleteWhere(sessionBuilder.build(),
+              where: (t) => t.email.equals(email));
+          expect(
+            () => endpoints.auth.sendResetPasswordOtp(sessionBuilder, email),
+            throwsA(
+              isA<AuthException>(),
+            ),
+          );
+        },
+      );
+    });
   });
 }
